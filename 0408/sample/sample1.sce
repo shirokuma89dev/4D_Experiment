a00c84b01e0cf57e237b4dea15a9c654b4f4d8f8
shirokuma89dev <code@shirokuma89.dev>
2024-04-15 15:42:23 +0900

    ✅ 第1回提出完了

---
+++ 0408/sample/sample1.sce
@@ -0,0 +1,5 @@
+t=0:0.1:10;
+y=sin(t);
+plot(t,y)
+xgrid
+xtitle('Sample','Time [sec]','Output[m]]')
---
+++ 0408/sample/sample3/sample3.ino
@@ -0,0 +1,15 @@
+#include <MsTimer2.h>
+
+void control() {
+
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  MsTimer2::set(20, control);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
